5051abefff4f9a1876263d0a8e95d89412a35868
qarisaab <imadalione@pm.me>
2023-10-08 21:07:27 +0500

    weather description fixed

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ search.addEventListener('click', ()=> {
                     image.src = ''
             }
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
-            desc.innerHTML = `${json.weather[0].desc}`
+            desc.innerHTML = `${json.weather[0].description}`
             humidity.innerHTML = `${json.main.humidity}%`
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
 
